244bccef0e99fa04c0b433335da416c3e2abf1bf
jiny3 <101464224+jiny3@users.noreply.github.com>
2025-08-03 21:36:08 +0800

    ✨ feat: rename package from service to services

--- bootstrap/start.go
+++ bootstrap/start.go
@@ -7,14 +7,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 
-	"github.com/jiny3/go-app/service"
+	"github.com/jiny3/go-app/services"
 )
 
 // Start start the application.
 func Start() {
 	app := APP("config/app.toml")
 	r := gin.Default()
-	r.GET("/hello", service.Hello)
+	r.GET("/hello", services.Hello)
 
 	if err := r.Run(strings.Join([]string{":", strconv.Itoa(app.Port)}, "")); err != nil {
 		logrus.WithError(err).Fatal("failed to start server")
